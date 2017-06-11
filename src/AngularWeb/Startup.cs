7669b6630669467d780b18c3172857048e345d33
patrick fennell <pafennell@gmail.com>
2017-06-11 16:39:44 +0100

    first commit

---
+++ src/AngularWeb/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace AngularWeb
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.json.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+           
+        }
+        public IConfigurationRoot Configuration { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+            if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value)){
+                    context.Request.Path = "/index.html";
+                    await next();
+            }
+            });
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
---
+++ src/AngularWeb/wwwroot/app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>My app AngularJs 2</h1>`,
+})
+export class AppComponent {
+}
